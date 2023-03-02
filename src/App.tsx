75d598580170191eb45da8d324d5f5335ce378fe
Miguel-hub22 <miguelrodrigues23@outlook.com.br>
2023-03-02 13:18:36 -0300

    Atualização: modificação dos conponentes Home, Login, Cadastro, Navbar, Footer

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,25 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Footer from './components/estaticos/footer/Footer';
+import Navbar from './components/estaticos/navbar/Navbar';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './paginas/home/Home';
+import Login from './paginas/login/Login';
+import Cadastro from './paginas/cadastro/Cadastro';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route path='/'element={<Home/>}/>
+        <Route path='/home'element={<Home/>}/>
+        <Route path='/login'element={<Login/>}/>
+        <Route path='/cadastro'element={<Cadastro/>}/>
+      </Routes>
+        <Footer />
+    </Router>
+
   );
 }
 
